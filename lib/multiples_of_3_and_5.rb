978a5328cecd342aa45965bb6502235881170b5a
Josh Hill <jamesjoshuahill@gmail.com>
2013-08-30 11:16:22 +0100

    Wrote working class to solve problem using TDD/Rspec

---
+++ lib/multiples_of_3_and_5.rb
@@ -0,0 +1,24 @@
+class Multiples3and5
+  def initialize(n)
+    @n = n
+  end
+
+  def multiple_of_3?(n)
+    n % 3 == 0
+  end
+
+  def multiple_of_5?(n)
+    n % 5 == 0
+  end
+
+  def multiples_of_3_or_5
+    1.upto(@n).select { |n| n if multiple_of_3?(n) || multiple_of_5?(n) }
+  end
+
+  def sum_of_multiples_of_3_or_5
+    multiples_of_3_or_5.inject(:+)
+  end
+end
+
+#simple execution test
+puts Multiples3and5.new(1000).sum_of_multiples_of_3_or_5
---
+++ spec/multiples_of_3_and_5_spec.rb
@@ -0,0 +1,33 @@
+require 'multiples_of_3_and_5'
+
+describe Multiples3and5 do
+  let(:multiple_10) { Multiples3and5.new(10) }
+  let(:multiple_5) { Multiples3and5.new(5) }
+
+  it 'should know if a number is a multiple of 3' do
+    expect(multiple_10.multiple_of_3?(3)).to be_true
+  end
+
+  it 'should know if a number is NOT a multiple of 3' do
+    expect(multiple_10.multiple_of_3?(1)).to be_false
+  end
+
+  it 'should know if a number is a multiple of 5' do
+    expect(multiple_10.multiple_of_5?(5)).to be_true
+  end
+
+  it 'should know if a number is NOT a multiple of 5' do
+    expect(multiple_10.multiple_of_5?(1)).to be_false
+  end
+
+  it 'should list the multiples of 3 or 5 up to the given number' do
+    expect(multiple_10.multiples_of_3_or_5).to eq [3, 5, 6, 9, 10]
+    expect(multiple_5.multiples_of_3_or_5).to eq [3, 5]
+  end
+
+  it 'should sum the multiples of 3 or 5 up to the given number' do
+    expect(multiple_10.sum_of_multiples_of_3_or_5).to eq 33
+    expect(multiple_5.sum_of_multiples_of_3_or_5).to eq 8
+  end
+
+end
